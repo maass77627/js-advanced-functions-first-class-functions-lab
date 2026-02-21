cdeea4d9a7071f81ca130194d4679d8086065158
Kelly Maass <kmaass77627@gmail.com>
2026-02-20 20:56:33 -0600

    done

--- index.js
+++ index.js
@@ -1,46 +1,5 @@
 
 
-// function returnFirstTwoDrivers(array){
-//    return array.slice(0,2)
-
-// }
-
-// function returnLastTwoDrivers(array) {
-//   let num = array.length
-//     let numtwo = array.length - 2
-//     return array.slice(numtwo, num)
-// }
-// const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers] 
-       
-
-// function createFareMultiplier(num, fare) {
-//     return (fare) => fare * num
-
-// }
-
-
-//  function fareDoubler(fare) {
-//     return fare*2
-
-//  }
-
-// function fareTripler(fare) {
-//     return fare*3
-
-// }
-
-// function selectDifferentDrivers(array, cb){
-//     return cb(array)
-
-// }
-
-
-
-
-
-
-
-
 
 const returnFirstTwoDrivers = function (drivers) {
    return drivers.slice(0,2)
